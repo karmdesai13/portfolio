2bc36eade1074ff0f76ebccdebc9ac0e964b0e1b
karmdesai13 <karmdushyantkumar.desai@edu.sait.ca>
2023-12-05 11:42:10 -0700

    Update

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -5,86 +5,86 @@ import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
 
 const projectsData = [
-    {
-      id: 1,
-      title: "React Portfolio Website",
-      description: "Project 1 description",
-      image: "/images/G.png",
-      tag: ["All", "Web"],
-      gitUrl: "/",
-      previewUrl: "/",
-    },
-    {
-      id: 2,
-      title: "Potography Portfolio Website",
-      description: "Project 2 description",
-      image: "/images/Ecommerce.png",
-      tag: ["All", "Web"],
-      gitUrl: "/",
-      previewUrl: "/",
-    },
-    {
-      id: 3,
-      title: "E-commerce Application",
-      description: "Project 3 description",
-      image: "/images/Gujju.png",
-      tag: ["All", "Web"],
-      gitUrl: "/",
-      previewUrl: "/",
-    },
-    {
-      id: 4,
-      title: "Food Ordering Application",
-      description: "Project 4 description",
-      image: "/images/S.png",
-      tag: ["All", "Mobile"],
-      gitUrl: "/",
-      previewUrl: "/",
-    },
-    {
-      id: 5,
-      title: "React Firebase Template",
-      description: "Authentication and CRUD operations",
-      image: "/images/S.png",
-      tag: ["All", "Web"],
-      gitUrl: "/",
-      previewUrl: "/",
-    },
-    {
-      id: 6,
-      title: "Full-stack Roadmap",
-      description: "Project 5 description",
-      image: "/images/G.png",
-      tag: ["All", "Web"],
-      gitUrl: "/",
-      previewUrl: "/",
-    },
-  ];
-const Projects = () => {
+  {
+    id: 1,
+    title: "React Portfolio Website",
+    description: "Project 1 description",
+    image: "/images/G.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 2,
+    title: "Potography Portfolio Website",
+    description: "Project 2 description",
+    image: "/images/G.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "E-commerce Application",
+    description: "Project 3 description",
+    image: "/images/G.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "Food Ordering Application",
+    description: "Project 4 description",
+    image: "/images/G.png",
+    tag: ["All", "Mobile"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 5,
+    title: "React Firebase Template",
+    description: "Authentication and CRUD operations",
+    image: "/images/G.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "Full-stack Roadmap",
+    description: "Project 5 description",
+    image: "/images/G.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+];
 
-    const [tag, setTag] = useState("All");
-    const ref=useRef(null);
-    const isInView=useInView(ref,{once:true});
-    
-  
-    const handleTagChange = (newTag) => {
-      setTag(newTag);
-    };
-  
-    const filteredProjects = projectsData.filter((project) =>
-      project.tag.includes(tag)
-    );
+const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
-    const cardVariants = {
-      initial: { y: 50, opacity: 0 },
-      animate: { y: 0, opacity: 1 },
-    };
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
 
   return (
-    <section ref={ref}>
-    <h2 className="text-white text-4xl text-center font-bold mt-4 mb-10"> My Projects</h2>
-    <div className="flex flex-row text-white justify-center items-center gap-2 py-6 ">
-    
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag
           onClick={handleTagChange}
           name="All"
@@ -100,30 +100,29 @@ const Projects = () => {
           name="Mobile"
           isSelected={tag === "Mobile"}
         />
-
-    </div>
-    <ul className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {filteredProjects.map((project,index) => (
-          <motion.li variants={cardVariants} initial="initial"
-          animate={isInView ? "animate" : "initial"}
-          transition={{ duration: 0.3, delay: index * 0.4 }}>
-           
-            <ProjectCard 
+      </div>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
               key={project.id}
-              imgUrl={project.image}
               title={project.title}
               description={project.description}
-                gitUrl={project.gitUrl}
-                previewUrl={project.previewUrl}
-              
-              
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
             />
-            </motion.li>
-          
+          </motion.li>
         ))}
+      </ul>
+    </section>
+  );
+};
 
-    </ul></section>
-  )
-}
-
-export default Projects;
+export default ProjectsSection;
