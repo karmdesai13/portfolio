3de3ee65a0f0f36f6d38f422ce095635a0aa31ea
karmdesai13 <karmdushyantkumar.desai@edu.sait.ca>
2023-12-05 11:39:42 -0700

    Update Hero.jsx

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
             <motion.div  initial={{opacity:0,scale:0.5}} animate={{opacity:1,scale:1}} transition={{duration:0.2}} className="col-span-7 place-self-center text-center sm:text-left ">
                 <h1 className="text-white mb-4 sm:text-5xl lg:text-8xl lg:leading-normal justify-self-start text-4xl font-extrabold">
                 <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-                    Hello, I'm
+                    Hello, I&apos;m{" "}
                 </span>
 
                    <br></br>
